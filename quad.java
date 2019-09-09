fdf7ae0c63537b5c4ccb07f907271718ff25df52
mms00459 <tdyakov@mm-sol.com>
2019-09-09 12:08:15 +0300

    now works with arbitrary sized images not rectangular only

--- quad.java
+++ quad.java
@@ -11,12 +11,12 @@ public class quad {
 	static int threshold = 45;
 	static boolean circle;
 	
-	static double entropy(BufferedImage img, int x, int y, int sz) {
+	static double entropy(BufferedImage img, int x, int y, int szX, int szY) {
 
 		double s = 0;
-		Color avg = avg(img, x, y, sz);
-		for (int i = x; i < x + sz; i++) {
-			for (int j = y; j < y + sz; j++) {
+		Color avg = avg(img, x, y, szX, szY);
+		for (int i = x; i < x + szX; i++) {
+			for (int j = y; j < y + szY; j++) {
 				Color c = new Color(img.getRGB(i, j));
 				int red = c.getRed();
 				int green = c.getGreen();
@@ -26,36 +26,36 @@ public class quad {
 				s += Math.abs(avg.getBlue() - blue);
 			}
 		}
-		return (double) s / (sz * sz);
+		return (double) s / (szX * szY);
 	}
 
-	static void fill(BufferedImage img, int x, int y, int sz, BufferedImage out) {
-		Color avg = avg(img, x, y, sz);
+	static void fill(BufferedImage img, int x, int y, int szX, int szY, BufferedImage out) {
+		Color avg = avg(img, x, y, szX, szY);
 
 		Graphics g = out.getGraphics();
 		g.setColor(avg);
 		if(circle){
-			g.fillOval(x,y, sz, sz);
+			g.fillOval(x,y, szX, szY);
 		}else{
-			g.fillRect(x, y, sz, sz);
+			g.fillRect(x, y, szX, szY);
 		}
 		g.dispose();
 	}
 
-	static void drawRectangle(BufferedImage img, int x, int y, int sz, BufferedImage out) {
+	static void drawRectangle(BufferedImage img, int x, int y, int szX, int szY, BufferedImage out) {
 		Graphics g = out.getGraphics();
 		g.setColor(Color.BLACK);
-		g.drawRect(x, y, sz, sz);
+		g.drawRect(x, y, szX, szY);
 		g.dispose(); 
 	}
 
-	static Color avg(BufferedImage img, int x, int y, int sz) {
+	static Color avg(BufferedImage img, int x, int y, int szX, int szY) {
 		int sumRed = 0;
 		int sumGreen = 0;
 		int sumBlue = 0;
 		int count = 0;
-		for (int i = x; i < x + sz; i++) {
-			for (int j = y; j < y + sz; j++) {
+		for (int i = x; i < x + szX; i++) {
+			for (int j = y; j < y + szY; j++) {
 				int red = new Color(img.getRGB(i, j)).getRed();
 				int green = new Color(img.getRGB(i, j)).getGreen();
 				int blue = new Color(img.getRGB(i, j)).getBlue();
@@ -68,24 +68,24 @@ public class quad {
 		return new Color(sumRed / count, sumGreen / count, sumBlue / count);
 	}
 
-	static void rec(BufferedImage img, int x, int y, int sz, BufferedImage out) {
-		if (sz <= 4) {
-			fill(img, x, y, sz, out);
+	static void rec(BufferedImage img, int x, int y, int szX, int szY, BufferedImage out) {
+		if (szX <= 4 || szY <= 4) {
+			fill(img, x, y, szX, szY, out);
 			if(!circle){
-				drawRectangle(img, x, y, sz, out);
+				drawRectangle(img, x, y, szX, szY, out);
 			}
 			return;
 		}
 
-		if (entropy(img, x, y, sz) > threshold) {
-			rec(img, x + sz / 2, y, sz / 2, out);
-			rec(img, x + sz / 2, y + sz / 2, sz / 2, out);
-			rec(img, x, y, sz / 2, out);
-			rec(img, x, y + sz / 2, sz / 2, out);
+		if (entropy(img, x, y, szX, szY) > threshold) {
+			rec(img, x + szX / 2, y, szX / 2, szY / 2, out);
+			rec(img, x + szX / 2, y + szY / 2, szX / 2, szY / 2, out);
+			rec(img, x, y, szX / 2, szY / 2, out);
+			rec(img, x, y + szY / 2, szX / 2, szY / 2, out);
 		} else {
-			fill(img, x, y, sz, out);
+			fill(img, x, y, szX, szY, out);
 			if(!circle){
-				drawRectangle(img, x, y, sz, out);
+				drawRectangle(img, x, y, szX, szY, out);
 			}
 		}
 	}
@@ -98,7 +98,7 @@ public class quad {
 		}
 
 		if(args.length >= 3){
-			if(args[2].equals("c")){
+			if(args[2].equals("-c")){
 				circle = true;
 			}
 		}
@@ -112,8 +112,9 @@ public class quad {
 		g.dispose();
 
 		int w = image.getWidth();
+		int h = image.getHeight();
 		System.out.println(w +" " + image.getHeight());
-		rec(image, 0, 0, w, out);
+		rec(image, 0, 0, w, h, out);
 
 		ImageIO.write(out, "png", new File("output.png"));
 	}
